30a8082b80b881e565af2ce5adb587fca90b094d
dhalfks <dhalfks0228@gmail.com>
2025-10-30 17:46:24 +0900

    1030 add

---
+++ test/test.js
@@ -0,0 +1,39 @@
+// 환전고시환율 []
+      const exs = {
+        exchanges : [
+        {
+          name:'미국USD',
+          price:1434.20,
+          updown:0.70
+        },
+        {
+          name:'일본JPY(100엔)',
+          price:941.20,
+          updown:3.11
+        },
+        {
+          name:'유럽연합EUR',
+          price:1672.49,
+          updown:2.89
+        },
+        {
+          name:'중국CNY',
+          price:201.96,
+          updown:0.30
+        }
+      ],
+
+      // 금시세
+      gold : [
+        {
+          name:'국제금(10.27)',
+          price:4019.70,
+          updown:-118.10
+        },
+        {
+          name:'국내금(10.28)',
+          price:184841.32,
+          updown:604.18
+        }
+      ]
+    };
